1f06bf1575993cf55989c641680eb1078e6db891
Pyaive Oleg <pyaive.oleg@yandex.ru>
2019-09-21 19:18:15 +0300

    Added first version of solution

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void representationOfNumber(int remainingPart, int maximalTerm, int numberOfCurrentTerm, int* currentRepresentation)
+{
+    if (remainingPart < 0)
+    {
+        return;
+    }
+    if (remainingPart == 0)
+    {
+        printf("%d ", currentRepresentation[0]);
+        for (int j = 1; j < numberOfCurrentTerm; j++)
+        {
+            printf("+ %d ", currentRepresentation[j]);
+        }
+        printf("\n");
+    }
+    else
+    {
+        if (remainingPart >= maximalTerm)
+        {
+            currentRepresentation[numberOfCurrentTerm] = maximalTerm;
+            representationOfNumber(remainingPart - maximalTerm, maximalTerm, numberOfCurrentTerm + 1, currentRepresentation);
+        }
+        if (maximalTerm > 1) {
+            representationOfNumber(remainingPart, maximalTerm - 1, numberOfCurrentTerm, currentRepresentation);
+        }
+    }
+    return;
+}
+
+int main()
+{
+    const int maxNumber = 100;
+    int currentRepresentation[maxNumber];
+    for (int i = 0; i < maxNumber; i++)
+    {
+        currentRepresentation[i] = 0;
+    }
+    int number = 0;
+
+    printf("Please, write down number (N): ");
+    scanf("%d", &number);
+
+    representationOfNumber(number, number, 0, currentRepresentation);
+
+    return 0;
+}
