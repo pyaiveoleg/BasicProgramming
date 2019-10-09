01e3e45796a7f52a66011e80d39dcd8aac24990b
Pyaive Oleg <pyaive.oleg@yandex.ru>
2019-10-09 20:53:36 +0300

    Переместил в нужную папку

---
+++ Semester_1/Homework_2/Task_2-4/2-4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int compare(const void * x1, const void * x2)   // функция сравнения элементов массива
+{
+    return ( *(int*)x1 - *(int*)x2 );
+}
+
+int main() {
+    const int maxArraySize = 1000;
+    int array[maxArraySize];
+    for (int i = 0; i < maxArraySize; i++)
+    {
+        array[i] = 0;
+    }
+    int maxRepeatingElement = 0;
+    int arrayLength = 0;
+    bool isFound = false;
+
+    printf("Please, write down length of the array: ");
+    scanf("%d", &arrayLength);
+
+    printf("Please, write down array: ");
+    for (int i = 0; i < arrayLength; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+
+    qsort(array, arrayLength, sizeof(int), compare);
+
+    int checkingElement = arrayLength - 1;
+    while (!isFound)
+    {
+        if (array[checkingElement] == array[checkingElement - 1])
+        {
+            maxRepeatingElement = array[checkingElement];
+            isFound = true;
+        }
+        checkingElement--;
+    }
+
+    printf("There is maximal repeating element in this array: %d", maxRepeatingElement);
+
+    return 0;
+}
