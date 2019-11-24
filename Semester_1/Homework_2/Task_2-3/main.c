47beee4d168d1753324e219d7a62a3bbdd6d0647
pyaiveoleg <pyaive.oleg@yandex.ru>
2019-11-24 03:56:58 +0300

    Decomposed into functions; removed mistakes with cows

--- Semester_1/Homework_2/Task_2-3/main.c
+++ Semester_1/Homework_2/Task_2-3/main.c
@@ -3,24 +3,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+void createThoughtNumber(int* thoughtNumber, bool* isDigitUsed)
 {
-    const int quantityOfDigits = 10;
     srand(time(NULL));
+    int randomDigit = 1 + rand() % 9;
+    isDigitUsed[randomDigit] = true;
+    thoughtNumber[0] = randomDigit;
 
-    bool isDigitUsed[quantityOfDigits];
-    bool isDigitUsedInGuessedNumber[quantityOfDigits];
-    bool isDigitABull[quantityOfDigits];
-    for (int i = 0; i < quantityOfDigits; i++)
-    {
-        isDigitUsed[i] = false;
-        isDigitUsedInGuessedNumber[i] = false;
-        isDigitABull[i] = false;
-    }
-
-    int thoughtNumber[4] = {0, 0, 0, 0};
-    int randomDigit = 0;
-    for (int i = 0; i <= 3; i++)
+    for (int i = 1; i <= 3; i++)
     {
         do
         {
@@ -31,6 +21,17 @@ int main()
         thoughtNumber[i] = randomDigit;
         isDigitUsed[randomDigit] = true;
     }
+}
+
+void play(int* thoughtNumber, const int quantityOfDigits, const bool* isDigitUsed)
+{
+    bool isDigitUsedInGuessedNumber[quantityOfDigits];
+    bool isDigitABull[quantityOfDigits];
+    for (int i = 0; i < quantityOfDigits; i++)
+    {
+        isDigitUsedInGuessedNumber[i] = false;
+        isDigitABull[i] = false;
+    }
 
     bool isSolved = false;
     int guessedNumber[4] = {0, 0, 0, 0};
@@ -39,51 +40,95 @@ int main()
     int bulls = 0;
     int turn = 1;
 
+    printf("I thought: %d%d%d%d\n", thoughtNumber[0], thoughtNumber[1], thoughtNumber[2], thoughtNumber[3]);
     printf("     Guess Result\n");
     while (!isSolved)
     {
         printf(" %d  |", turn);
         scanf("%d", &inputNumber);
+        bool fourDigitsInGuessedNumber = true;
+        if (inputNumber >= 10000 || inputNumber < 1000)
+        {
+            printf("\nThere must be 4 digits in your number, please rewrite\n");
+            fourDigitsInGuessedNumber = false;
+        }
+        bool allDigitsAreDifferent = true;
 
         for (int i = 3; i >= 0; i--)
         {
             int currentDigit = inputNumber % 10;
             guessedNumber[i] = currentDigit;
-            isDigitUsedInGuessedNumber[currentDigit] = true;
+
+            if (isDigitUsedInGuessedNumber[currentDigit])
+            {
+                printf("\nAll digits need to be different, please rewrite:\n");
+                allDigitsAreDifferent = false;
+                break;
+            }
+            else
+            {
+                isDigitUsedInGuessedNumber[currentDigit] = true;
+            }
             inputNumber /= 10;
         }
 
-        for (int i = 0; i <= 3; i++)
+        if (allDigitsAreDifferent && fourDigitsInGuessedNumber)
         {
-            if (guessedNumber[i] == thoughtNumber[i])
+            for (int i = 0; i <= 3; i++)
             {
-                bulls++;
-                isDigitABull[i] = true;
+                if (guessedNumber[i] == thoughtNumber[i])
+                {
+                    bulls++;
+                    isDigitABull[guessedNumber[i]] = true;
+                }
             }
+
+            for (int i = 0; i < quantityOfDigits; i++)
+            {
+                if (isDigitUsed[i] && isDigitUsedInGuessedNumber[i] && (!isDigitABull[i]))
+                {
+                    cows++;
+                }
+            }
+
+            printf("           %d cows %d bulls\n", cows, bulls);
+
+            if (bulls == 4)
+            {
+                isSolved = true;
+            }
+            turn++;
         }
 
         for (int i = 0; i < quantityOfDigits; i++)
         {
-            if ((isDigitUsed[i]) && (isDigitUsedInGuessedNumber[i]) && (!isDigitABull[i]))
-            {
-                cows++;
-            }
-            isDigitABull[i] = false;
             isDigitUsedInGuessedNumber[i] = false;
-        }
-
-        printf("           %d cows %d bulls\n", cows, bulls);
-
-        if (bulls == 4)
-        {
-            isSolved = true;
+            isDigitABull[i] = false;
         }
         bulls = 0;
         cows = 0;
-        turn++;
     }
 
     printf("You win!");
+}
+
+int main()
+{
+    const int quantityOfDigits = 10;
+
+    bool isDigitUsed[quantityOfDigits];
+    bool isDigitUsedInGuessedNumber[quantityOfDigits];
+    bool isDigitABull[quantityOfDigits];
+    for (int i = 0; i < quantityOfDigits; i++)
+    {
+        isDigitUsed[i] = false;
+        isDigitUsedInGuessedNumber[i] = false;
+        isDigitABull[i] = false;
+    }
+    int thoughtNumber[4] = {0, 0, 0, 0};
+
+    createThoughtNumber(thoughtNumber, isDigitUsed);
+    play(thoughtNumber, quantityOfDigits, isDigitUsed);
 
     return 0;
 }
