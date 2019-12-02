bdeb209f780ce40fbf20b524ede4f7800fc15b8c
pyaiveoleg <pyaive.oleg@yandex.ru>
2019-12-02 15:40:12 +0300

    Added error handling

--- Semester_1/Homework_8/Task_1/main.c
+++ Semester_1/Homework_8/Task_1/main.c
@@ -19,6 +19,49 @@ void divideIntoComponentsByDFS(int numberOfVertex, bool* used, const int quantit
     }
 }
 
+void readInput(bool** areWorksEqual, const int quantityOfStudents)
+{
+    int numberOfStudent = 0;
+    int numberOfOriginalWork = 0;
+
+    printf("Please, write down number of student and number of original work:\n");
+    for (int i = 0; i < quantityOfStudents; ++i)
+    {
+        scanf("%d %d", &numberOfStudent, &numberOfOriginalWork);
+        if (numberOfOriginalWork > quantityOfStudents || numberOfStudent > quantityOfStudents)
+        {
+            printf("Number of student cannot be bigger than quantity of students.\n");
+            i--;
+            continue;
+        }
+
+        if (numberOfOriginalWork == -1)
+        {
+            areWorksEqual[numberOfStudent][0] = true;
+            areWorksEqual[0][numberOfStudent] = true;
+        }
+        else
+        {
+            areWorksEqual[numberOfStudent][numberOfOriginalWork] = true;
+            areWorksEqual[numberOfOriginalWork][numberOfStudent] = true;
+        }
+    }
+}
+
+void printAnswer(bool* used, const int quantityOfStudents, bool** areWorksEqual)
+{
+    printf("These students haven't done anything: ");
+    divideIntoComponentsByDFS(0, used, quantityOfStudents, areWorksEqual);
+    printf("\n");
+
+    for (int i = 1; i <= 3; ++i)
+    {
+        printf("These students copied work of %d student: ", i);
+        divideIntoComponentsByDFS(i, used, quantityOfStudents, areWorksEqual);
+        printf("\n");
+    }
+}
+
 int main()
 {
     int quantityOfStudents = 0;
@@ -39,24 +82,7 @@ int main()
         }
     }
 
-    int numberOfStudent = 0;
-    int numberOfOriginalWork = 0;
-
-    printf("Please, write down number of student and number of original work:\n");
-    for (int i = 0; i < quantityOfStudents; ++i)
-    {
-        scanf("%d %d", &numberOfStudent, &numberOfOriginalWork);
-        if (numberOfOriginalWork == -1)
-        {
-            areWorksEqual[numberOfStudent][0] = true;
-            areWorksEqual[0][numberOfStudent] = true;
-        }
-        else
-        {
-            areWorksEqual[numberOfStudent][numberOfOriginalWork] = true;
-            areWorksEqual[numberOfOriginalWork][numberOfStudent] = true;
-        }
-    }
+    readInput(areWorksEqual, quantityOfStudents);
 
     bool used[quantityOfStudents + 1];
     for (int i = 0; i <= quantityOfStudents; ++i)
@@ -64,16 +90,7 @@ int main()
         used[i] = false;
     }
 
-    printf("These students haven't done anything: ");
-    divideIntoComponentsByDFS(0, used, quantityOfStudents, areWorksEqual);
-    printf("\n");
-
-    for (int i = 1; i <= 3; ++i)
-    {
-        printf("These students copied work of %d student: ", i);
-        divideIntoComponentsByDFS(i, used, quantityOfStudents, areWorksEqual);
-        printf("\n");
-    }
+    printAnswer(used, quantityOfStudents, areWorksEqual);
 
     for (int i = 0; i <= quantityOfStudents; ++i)
     {
