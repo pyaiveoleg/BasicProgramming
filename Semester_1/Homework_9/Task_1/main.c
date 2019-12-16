59f2b3f033934296f76928a0feac19559ef63293
pyaiveoleg <pyaive.oleg@yandex.ru>
2019-12-16 19:53:10 +0300

    Renamed States, created functions for parsing

--- Semester_1/Homework_9/Task_1/main.c
+++ Semester_1/Homework_9/Task_1/main.c
@@ -4,6 +4,21 @@
 #include <string.h>
 #include <ctype.h>
 
+typedef enum State State;
+enum State
+{
+    start,
+    firstDigit,
+    integerPart,
+    dotInMantissa,
+    fractionalPart,
+    charE,
+    firstDigitOfExponent,
+    integerPartOfExponent,
+    success,
+    error
+};
+
 bool isDigit(char symbol)
 {
     return (symbol >= '0') && (symbol <= '9');
@@ -30,10 +45,134 @@ char* readString(size_t startingSizeOfString)
     return currentString;
 }
 
+void signParsing(char currentSymbol, State* state)
+{
+    if (currentSymbol == '+' || currentSymbol == '-')
+    {
+        *state = firstDigit;
+    }
+    else if (isDigit(currentSymbol))
+    {
+        *state = integerPart;
+    }
+    else
+    {
+        *state = error;
+    }
+}
+
+void firstDigitParsing(char currentSymbol, State* state)
+{
+    if (isDigit(currentSymbol))
+    {
+        *state = integerPart;
+    }
+    else
+    {
+        *state = error;
+    }
+}
+
+void integerPartParsing(char currentSymbol, State* state)
+{
+    if (isDigit(currentSymbol))
+    {
+        return; //state неизменный
+    }
+    else if (currentSymbol == '\0')
+    {
+        *state = success;
+    }
+    else if (currentSymbol == '.')
+    {
+        *state = dotInMantissa;
+    }
+    else
+    {
+        *state = error;
+    }
+}
+
+void dotInMantissaParsing(char currentSymbol, State* state)
+{
+    if (isDigit(currentSymbol))
+    {
+        *state = fractionalPart;
+    }
+    else
+    {
+        *state = error;
+    }
+}
+
+void fractionalPartParsing(char currentSymbol, State* state)
+{
+    if (isDigit(currentSymbol))
+    {
+        return; //state неизменный
+    }
+    else if (currentSymbol == '\0')
+    {
+        *state = success;
+    }
+    else if (currentSymbol == 'E')
+    {
+        *state = charE;
+    }
+    else
+    {
+        *state = false;
+    }
+}
+
+void charEParsing(char currentSymbol, State* state)
+{
+    if (currentSymbol == '+' || currentSymbol == '-')
+    {
+        *state = firstDigitOfExponent;
+    }
+    else if (isDigit(currentSymbol))
+    {
+        *state = integerPartOfExponent;
+    }
+    else
+    {
+        *state = error;
+    }
+}
+
+void firstDigitOfExponentParsing(char currentSymbol, State* state)
+{
+    if (isDigit(currentSymbol))
+    {
+        *state = integerPartOfExponent;
+    }
+    else
+    {
+        *state = false;
+    }
+}
+
+void integerPartOfExponentParsing(char currentSymbol, State* state)
+{
+    if (isDigit(currentSymbol))
+    {
+        return; //state неизменный
+    }
+    else if (currentSymbol == '\0')
+    {
+        *state = true;
+    }
+    else
+    {
+        *state = false;
+    }
+}
+
 bool isRealNumber(char* inputString)
 {
     char currentSymbol = '\0';
-    int state = 0;
+    State state = start;
 
     for (int index = 0; index <= strlen(inputString); index++)
     {
@@ -42,117 +181,56 @@ bool isRealNumber(char* inputString)
         {
             continue;
         }
-        
         switch (state)
         {
-            case 0: //start
-                if (currentSymbol == '+' || currentSymbol == '-')
-                {
-                    state = 1;
-                    break;
-                }
-                else if (isDigit(currentSymbol))
-                {
-                    state = 2;
-                    break;
-                }
-                else
-                {
-                    return false;
-                }
-            case 1:
-                if (isDigit(currentSymbol))
-                {
-                    state = 2;
-                    break; //state неизменный
-                }
-                else
-                {
-                    return false;
-                }
-            case 2:
-                if (isDigit(currentSymbol))
-                {
-                    break; //state неизменный
-                }
-                else if (currentSymbol == '\0')
-                {
-                    return true;
-                }
-                else if (currentSymbol == '.')
-                {
-                    state = 3;
-                    break;
-                }
-                else
-                {
-                    return false;
-                }
-            case 3:
-                if (isDigit(currentSymbol))
-                {
-                    state = 4;
-                    break;
-                }
-                else
-                {
-                    return false;
-                }
-            case 4:
-                if (isDigit(currentSymbol))
-                {
-                    break;
-                }
-                else if (currentSymbol == '\0')
-                {
-                    return true;
-                }
-                else if (currentSymbol == 'E')
-                {
-                    state = 5;
-                    break;
-                }
-                else
-                {
-                    return false;
-                }
-            case 5:
-                if (currentSymbol == '+' || currentSymbol == '-')
-                {
-                    state = 6;
-                    break;
-                }
-                else if (isDigit(currentSymbol))
-                {
-                    state = 7;
-                    break;
-                }
-                else
-                {
-                    return false;
-                }
-            case 6:
-                if (isDigit(currentSymbol))
-                {
-                    state = 7;
-                }
-                else
-                {
-                    return false;
-                }
-            case 7:
-                if (isDigit(currentSymbol))
-                {
-                    break; //state неизменный
-                }
-                else if (currentSymbol == '\0')
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+            case start:
+            {
+                signParsing(currentSymbol, &state);
+                break;
+            }
+            case firstDigit:
+            {
+                firstDigitParsing(currentSymbol, &state);
+                break;
+            }
+            case integerPart:
+            {
+                integerPartParsing(currentSymbol, &state);
+                break;
+            }
+            case dotInMantissa:
+            {
+                dotInMantissaParsing(currentSymbol, &state);
+                break;
+            }
+            case fractionalPart:
+            {
+                fractionalPartParsing(currentSymbol, &state);
+                break;
+            }
+            case charE:
+            {
+                charEParsing(currentSymbol, &state);
+                break;
+            }
+            case firstDigitOfExponent:
+            {
+                firstDigitOfExponentParsing(currentSymbol, &state);
+                break;
+            }
+            case integerPartOfExponent:
+            {
+                integerPartOfExponentParsing(currentSymbol, &state);
+                break;
+            }
+            case success:
+            {
+                return true;
+            }
+            case error:
+            {
+                return false;
+            }
         }
     }
 
