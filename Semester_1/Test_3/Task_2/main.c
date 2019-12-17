3f3f36016b376e720ac81216139be1158774cc0c
pyaiveoleg <pyaive.oleg@yandex.ru>
2019-12-17 14:50:04 +0300

    Added handling of double, decomposed into functions

--- Semester_1/Test_3/Task_2/main.c
+++ Semester_1/Test_3/Task_2/main.c
@@ -17,6 +17,16 @@ double** createTwoDimensionalArray(int height, int width, int defaultValue)
     return newArray;
 }
 
+double* createOneDimensionalArray(int size, double defaultValue)
+{
+    double* newArray = (double*) malloc(sizeof(double) * size);
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = defaultValue;
+    }
+    return newArray;
+}
+
 void deleteTwoDimensionalArray(double** array, int height)
 {
     for (int i = 0; i < height; ++i)
@@ -26,32 +36,38 @@ void deleteTwoDimensionalArray(double** array, int height)
     free(array);
 }
 
-int main()
+void printSaddlePoints(double** array, const double* maxInColumn, const double* minInLine, int height, int width)
 {
-    int width = 0;
-    int height = 0;
-    printf("Please, write down width and height of array:\n");
-    scanf("%d %d", &width, &height);
-    double** array = createTwoDimensionalArray(height, width, 0);
-
-    double maxInColumn[width];
-    for (int i = 0; i < width; i++)
-    {
-        maxInColumn[i] = 0;
-    }
-
-    double minInLine[height];
+    printf("There are indexes (X Y) of elements (and elements itself) that are saddle points of array:\n");
     for (int i = 0; i < height; i++)
     {
-        minInLine[i] = 0;
-    }//createOneDimensionalArray - dont't forget!
+        for (int j = 0; j < width; j++)
+        {
+            if (array[i][j] == maxInColumn[j] && array[i][j] == minInLine[i])
+            {
+                printf("(%d %d %lf)\n", i, j, array[i][j]);
+            }
+        }
+    }
+}
 
-    printf("Please, write down the two-dimensional array:\n");
+void readArray(double** array, int height, int width)
+{
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             scanf("%lf", &array[i][j]);
+        }
+    }
+}
+
+void countMaxInColumnsAndMinInLines(double** array, int height, int width, double* maxInColumn, double* minInLine)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             if (array[i][j] > maxInColumn[j] || i == 0)//инциализируем maxInColumn[j] первым элементом столбца
             {
                 maxInColumn[j] = array[i][j];
@@ -62,19 +78,26 @@ int main()
             }
         }
     }
+}
 
-    printf("There are indexes (X Y) of elements that are saddle points of array:\n");
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            if (array[i][j] == maxInColumn[j] && array[i][j] == minInLine[i])
-            {
-                printf("(%d %d)\n", i, j);
-            }
-        }
-    }
+int main()
+{
+    int width = 0;
+    int height = 0;
+    printf("Please, write down width and height of array:\n");
+    scanf("%d %d", &width, &height);
+    double** array = createTwoDimensionalArray(height, width, 0);
+
+    double* maxInColumn = createOneDimensionalArray(width, 0);
+    double* minInLine = createOneDimensionalArray(height, 0);
+
+    printf("Please, write down the two-dimensional array:\n");
+    readArray(array, height, width);
+    countMaxInColumnsAndMinInLines(array, height, width, maxInColumn, minInLine);
+    printSaddlePoints(array, maxInColumn, minInLine, height, width);
 
     deleteTwoDimensionalArray(array, height);
+    free(maxInColumn);
+    free(minInLine);
     return 0;
 }
