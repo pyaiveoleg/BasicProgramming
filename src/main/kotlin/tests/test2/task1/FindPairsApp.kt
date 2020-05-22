e6018b7969ae67dfd28418e6bdd78b8a15f6fbf7
pyaiveoleg <pyaive.oleg@yandex.ru>
2020-05-22 19:02:54 +0300

    removed spread operator

--- src/main/kotlin/tests/test2/task1/FindPairsApp.kt
+++ src/main/kotlin/tests/test2/task1/FindPairsApp.kt
@@ -1,28 +1,23 @@
 package tests.test2.task1
 
 import javafx.application.Application
-import javafx.stage.Stage
 import tornadofx.App
 import tornadofx.find
 
-class FindPairsApp : App(StartView::class) {
-    override fun start(stage: Stage) {
-        val controller = find(MainController::class)
-        val size = parameters.named["size"]?.toInt()
-        if (size == null) {
-            print("You haven't passed size")
-            return
-        }
-        if (size % 2 == 1) {
-            print("Size cannot be odd")
-            return
-        }
-        controller.initializeBoard(size)
-        print(controller.size)
-        super.start(stage)
-    }
-}
+class FindPairsApp : App(StartView::class)
 
 fun main(args: Array<String>) {
-    Application.launch(FindPairsApp::class.java, *arrayOf(*args))
+    if (args.isEmpty()) {
+        print("You haven't passed size")
+        return
+    }
+    val size = args[0].toInt()
+    val controller = find(MainController::class)
+
+    if (size % 2 == 1) {
+        print("Size cannot be odd")
+        return
+    }
+    controller.initializeBoard(size)
+    Application.launch(FindPairsApp::class.java)
 }
