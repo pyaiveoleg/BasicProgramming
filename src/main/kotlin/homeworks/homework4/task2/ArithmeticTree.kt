a2d4d1099c7b2f662063664935d4aee66f98ccba
pyaiveoleg <pyaive.oleg@yandex.ru>
2020-06-07 16:25:21 +0300

    fixed detekt issues

--- src/main/kotlin/homeworks/homework4/task2/ArithmeticTree.kt
+++ src/main/kotlin/homeworks/homework4/task2/ArithmeticTree.kt
@@ -2,34 +2,37 @@ package homeworks.homework4.task2
 
 class ArithmeticTree(private val root: Node) {
     companion object ImportTree {
+        private const val shiftForOperator = 3
+
         fun importTree(stringWithTree: String): ArithmeticTree {
             return ArithmeticTree(importSubtree(0, stringWithTree.length - 1, stringWithTree))
         }
 
         private fun splitString(stringWithSubtree: String): Int {
             var quantityOfNonClosedBrackets = 0
+            var valueToReturn = 0
             if (stringWithSubtree[0].isDigit()) {
-                return stringWithSubtree.indexOf(' ')
+                valueToReturn = stringWithSubtree.indexOf(' ')
+            } else {
+                for (i in stringWithSubtree.indices) {
+                    if (stringWithSubtree[i] == '(') {
+                        quantityOfNonClosedBrackets++
+                    }
+                    if (stringWithSubtree[i] == ')') {
+                        quantityOfNonClosedBrackets--
+                    }
+                    if (quantityOfNonClosedBrackets == 0) {
+                        return i + 1
+                    }
+                }
             }
-            for (i in stringWithSubtree.indices) {
-                if (stringWithSubtree[i] == '(') {
-                    quantityOfNonClosedBrackets++
-                }
-                if (stringWithSubtree[i] == ')') {
-                    quantityOfNonClosedBrackets--
-                }
-                if (quantityOfNonClosedBrackets == 0) {
-                    return i + 1
-                }
-            }
-            return stringWithSubtree.length - 1
+            return valueToReturn
         }
 
         private fun importSubtree(start: Int, end: Int, stringWithTree: String): Node {
             if (stringWithTree[start] != '(') {
                 return NodeOperand(stringWithTree.slice(start..end).toDouble())
             }
-            val shiftForOperator = 3
             val stringForParse = stringWithTree.slice(start + shiftForOperator until end)
             val gapBetweenLeftAndRightNotation = splitString(stringForParse) + shiftForOperator + start
             val leftChild = importSubtree(start + shiftForOperator, gapBetweenLeftAndRightNotation - 1, stringWithTree)
