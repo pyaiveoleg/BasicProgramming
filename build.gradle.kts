11083d91cc40405cfcec03bd5790db23de697e82
pyaiveoleg <pyaive.oleg@yandex.ru>
2020-09-08 19:49:35 +0300

    Upgraded Gradle, Kotlin plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.3.70"
+    kotlin("jvm") version "1.4.0"
+    kotlin("plugin.serialization") version "1.4.0"
     id("io.gitlab.arturbosch.detekt") version "1.6.0"
     id("org.openjfx.javafxplugin") version "0.0.8"
-}
-
-javafx {
-    modules("javafx.controls", "javafx.fxml")
 }
 
 group = "org.example"
@@ -13,18 +10,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    jcenter ()
+    jcenter()
 }
 
 val ktor_version = "1.3.2"
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib"))
     implementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     implementation("org.junit.jupiter:junit-jupiter-engine:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
-    compile("io.ktor:ktor-server-netty:1.3.2")
-    compile("io.ktor:ktor-server-core:1.3.2")
     implementation("no.tornado:tornadofx:1.7.20")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.70")
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.6.0")
@@ -35,6 +30,7 @@ dependencies {
     implementation ("io.ktor:ktor-websockets:$ktor_version")
     implementation("org.junit.jupiter:junit-jupiter-params:5.0.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:0.9.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC")
 }
 
 detekt {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
-rootProject.name = "Semester 2"
+rootProject.name = "Semester3"
 
